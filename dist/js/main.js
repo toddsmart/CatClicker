97cf3500c93d7d73420afdac6204ee8605177687
todd <todd.smart@smartbinary.com>
2015-08-24 12:26:06 -0400

    version 2 - two cat click app

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,14 @@
-var numClicks = 0,
-    catImage = document.getElementById('cat-image'),
-    clickCounter = document.getElementById('click-counter');
+var numClicks1 = 0,
+    catImage1 = document.getElementById('cat-image1'),
+    clickCounter1 = document.getElementById('click-counter1'),
+    numClicks2 = 0,
+    catImage2 = document.getElementById('cat-image2'),
+    clickCounter2 = document.getElementById('click-counter2');
 
-catImage.addEventListener('click', function () {
-    clickCounter.innerText = (++numClicks).toString();
+catImage1.addEventListener('click', function () {
+    clickCounter1.innerText = (++numClicks1).toString();
+}, false);
+
+catImage2.addEventListener('click', function () {
+    clickCounter2.innerText = (++numClicks2).toString();
 }, false);
