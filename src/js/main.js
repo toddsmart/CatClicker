14fdf35093387144f37d16befcb9bf5f37936f1e
todd <todd.smart@smartbinary.com>
2015-08-24 14:49:41 -0400

    version 3 - N-numbers of cat click app

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,40 @@
-var numClicks1 = 0,
-    catImage1 = document.getElementById('cat-image1'),
-    clickCounter1 = document.getElementById('click-counter1'),
-    numClicks2 = 0,
-    catImage2 = document.getElementById('cat-image2'),
-    clickCounter2 = document.getElementById('click-counter2');
+var catIdx,
+    chooseCatListElement = document.getElementById('choose-cat-list'),
+    displayCatNameElement = document.getElementById('display-cat-name'),
+    displayCatImageElement = document.getElementById('display-cat-image'),
+    displayCatClickCountElement = document.getElementById('display-cat-click-count'),
+    catData = [
+    {
+        name : 'catA',
+        picture : 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+        clickCount : 0
+    }, {
+        name : 'catB',
+        picture : 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&amp;h=496',
+        clickCount : 0
+    }, {
+        name : 'catC',
+        picture : 'https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&amp;h=454',
+        clickCount : 0
+    }
+];
 
-catImage1.addEventListener('click', function () {
-    clickCounter1.innerText = (++numClicks1).toString();
-}, false);
+for (catIdx = 0; catIdx < catData.length; catIdx++) {
 
-catImage2.addEventListener('click', function () {
-    clickCounter2.innerText = (++numClicks2).toString();
-}, false);
+    (function (catNumber) {
+
+        var chooseCatElement = document.createElement('div');
+
+        chooseCatElement.innerText = catData[catNumber].name;
+        chooseCatListElement.appendChild(chooseCatElement);
+
+        chooseCatElement.addEventListener('click', function () {
+            var clickCount = ++catData[catNumber].clickCount;
+            displayCatNameElement.innerText = catData[catNumber].name;
+            displayCatImageElement.src = catData[catNumber].picture;
+            displayCatClickCountElement.innerText = clickCount.toString();
+        }, false);
+
+    }(catIdx));
+
+}
